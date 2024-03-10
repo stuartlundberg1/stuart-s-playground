5dae452b639aa1f18a732a33d9fbeae51f58402f
Stuart Lundberg <162913389+stuartlundberg1@users.noreply.github.com>
2024-03-10 14:23:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+def easy_area(tree_length):
+'''This is a simple program to determine the area of this square tree'''
+  new_list = []
+  for i in tree_length:
+    new_list.append(i * i)
+  return new_list
+
+i = [2, 4, 6, 8, 10, 12]
+print(easy_area(i))
+
+
+  
 
