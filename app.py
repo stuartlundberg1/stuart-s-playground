772d47c1fde051aeeb7e9574405ea362e9b2a045
Stuart Lundberg <162913389+stuartlundberg1@users.noreply.github.com>
2024-03-11 17:22:22 -0400

    Update app.py
    Improved Code

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-def easy_area(tree_length):
-'''This is a simple program to determine the area of this square tree'''
-  new_list = []
-  for i in tree_length:
-    new_list.append(i * i)
-  return new_list
+def calculate_square_tree_areas(side_lengths):
+    """
+    Calculate the area of square trees given their side lengths.
+    
+    Args:
+        side_lengths (List[int]): A list of integers representing the side lengths of square trees.
+    
+    Returns:
+        List[int]: A list of integers representing the areas of the square trees.
+    """
+    new_list = []
+    for length in side_lengths:
+        new_list.append(length**2)
+    
+    return (new_list)
 
-i = [2, 4, 6, 8, 10, 12]
-print(easy_area(i))
-
-
-  
-
+side_lengths = [20]
+areas = calculate_square_tree_areas(side_lengths)
+print(areas)
